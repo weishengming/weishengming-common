5da3d211f4bcb240a76d5ff3cc7d7ed1df5ab743
yangtianci <yangtianci@99fenqi.com>
2016-07-22 14:52:05 +0800

    修改结构

---
+++ src/main/java/com/weishengming/hessian/mannaservice/api/service/MannaService.java
@@ -0,0 +1,10 @@
+package com.weishengming.hessian.mannaservice.api.service;
+
+/**
+ * mannaService
+ * @author yangtianci
+ * @version $Id: MannaService.java, v 0.1 2016年7月22日 下午2:48:19 yangtianci Exp $
+ */
+public interface MannaService {
+
+}
