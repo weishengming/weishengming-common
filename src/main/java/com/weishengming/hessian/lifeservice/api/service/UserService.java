cb4773c8e293b631e6a35df18dbc4e7b5899b747
yangtianci <yangtianci@99fenqi.com>
2016-11-02 16:45:26 +0800

    aa

--- src/main/java/com/weishengming/hessian/lifeservice/api/service/UserService.java
+++ src/main/java/com/weishengming/hessian/lifeservice/api/service/UserService.java
@@ -6,9 +6,15 @@ package com.weishengming.hessian.lifeservice.api.service;
  * @version $Id: UserService.java, v 0.1 2016年10月25日 下午5:00:03 yangtianci Exp $
  */
 public interface UserService {
+
+    /**
+     * 检查用户的唯一性
+     */
+    public Boolean checkMobile(String mobile);
+
     /**
      * 保存用户
      */
-    public Integer saveUser(String trueName, String password, String idCard, String mobile);
+    public Integer saveUser(String mobile, String password);
 
 }
