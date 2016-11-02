c8880b63516d1cc05abeeac1a7fb8bc3161e670e
yangtianci <yangtianci@99fenqi.com>
2016-11-02 11:37:59 +0800

    aa

---
+++ src/main/java/com/weishengming/hessian/lifeservice/api/service/ArticleService.java
@@ -0,0 +1,10 @@
+package com.weishengming.hessian.lifeservice.api.service;
+
+/**
+ * 文章service
+ * @author yangtianci
+ * @version $Id: ArticleService.java, v 0.1 2016年11月2日 上午11:30:11 yangtianci Exp $
+ */
+public interface ArticleService {
+
+}
